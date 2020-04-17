e3d940df9e853daa82cf00d126630a71611cb892
Christian Nunciato <c@nunciato.org>
2020-04-17 08:23:15 -0700

    Initial commit

---
+++ 01/hello/index.ts
@@ -0,0 +1,16 @@
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.Bucket("hello-world", {
+    website: {
+        indexDocument: "index.html"
+    }
+});
+
+const homepage = new aws.s3.BucketObject("index.html", {
+    bucket: bucket,
+    acl: aws.s3.PublicReadAcl,
+    content: "<html>Hello, world.</html>",
+    contentType: "text/html"
+});
+
+export const url = bucket.websiteEndpoint;
