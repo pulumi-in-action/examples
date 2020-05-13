83220230b74e3251d7090c23d23fba4808ab7c5c
Christian Nunciato <c@nunciato.org>
2020-05-13 11:28:24 -0700

    Make configurable things configurable

--- 02/cloud-notifier/index.ts
+++ 02/cloud-notifier/index.ts
@@ -1,26 +1,26 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config();
+const scheduleExpression = config.require("schedule_expression");
+const phoneNumber = config.requireSecret("phone_number");
+const kidNames: string[] = config.requireObject("kid_names");
 
 const topic = new aws.sns.Topic("topic");
 
 const subscription = new aws.sns.TopicSubscription("subscription", {
     topic: topic,
     protocol: "sms",
-    endpoint: "+12065551212"
+    endpoint: phoneNumber,
 });
 
-const scheduleExpression = "rate(1 minute)";
-
 aws.cloudwatch.onSchedule("schedule", scheduleExpression, async () => {
-    const kids = ["Oliver", "Sam", "Rosemary"];
-    const shuffledKids = kids.sort(() => Math.random() > 0.5 ? -1 : 1).join(", ");
+    const shuffledKids = kidNames.sort(() => Math.random() > 0.5 ? -1 : 1).join(", ");
     const message = `This week's game-playing order: ${shuffledKids}. 🎉 🕹 👾`;
 
     const sns = new aws.sdk.SNS();
     await sns.publish(
-        {
-            Message: message,
-            TopicArn: topic.arn.get()
-        },
+        { Message: message, TopicArn: topic.arn.get() },
         (err, data) => console.log(err || data)
     ).promise();
 });
