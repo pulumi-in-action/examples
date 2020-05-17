cec832b310ba0412e714d10b99f88785cc19c86d
Christian Nunciato <c@nunciato.org>
2020-05-16 17:54:42 -0700

    Apply the code as of the end of chapter 2

--- 02/cloud-notifier/index.ts
+++ 02/cloud-notifier/index.ts
@@ -1,26 +1,26 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
-const config = new pulumi.Config();
-const scheduleExpression = config.require("schedule_expression");
-const phoneNumber = config.requireSecret("phone_number");
-const kidNames: string[] = config.requireObject("kid_names");
 
 const topic = new aws.sns.Topic("topic");
 
 const subscription = new aws.sns.TopicSubscription("subscription", {
     topic: topic,
     protocol: "sms",
-    endpoint: phoneNumber,
+    endpoint: "+12065551212"
 });
 
-aws.cloudwatch.onSchedule("schedule", scheduleExpression, async () => {
-    const shuffledKids = kidNames.sort(() => Math.random() > 0.5 ? -1 : 1).join(", ");
+const scheduleExpression = "cron(35 22 ? * SAT *)";
+
+const handler = aws.cloudwatch.onSchedule("handler", scheduleExpression, () => {
+    const kids = ["Oliver", "Sam", "Rosemary"];
+    const shuffledKids = kids.sort(() => Math.random() > 0.5 ? -1 : 1).join(", ");
     const message = `This week's game-playing order: ${shuffledKids}. 🎉 🕹 👾`;
 
     const sns = new aws.sdk.SNS();
-    await sns.publish(
-        { Message: message, TopicArn: topic.arn.get() },
-        (err, data) => console.log(err || data)
-    ).promise();
+    sns.publish({
+        Message: message,
+        TopicArn: topic.arn.get()
+    })
+    .on("success", (response) => console.log(response.data))
+    .on("error", (error) => console.error(error))
+    .send();
 });
