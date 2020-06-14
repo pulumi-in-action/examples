8eeeee899708c00126b8cd2cd605c6728e4010e3
Christian Nunciato <c@nunciato.org>
2020-06-14 15:16:35 -0700

    Make the examples build properly

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -5,6 +5,9 @@ source ./scripts/common.sh
 for project in ${PROJECTS}
 do
     pushd "${project}"
+        stack="pulumi-in-action/${project/\//-}/dev"
+        pulumi stack init ${stack} || true
+        pulumi stack select ${stack}
         pulumi destroy --skip-preview --yes
     popd
 done
--- scripts/up.sh
+++ scripts/up.sh
@@ -5,9 +5,11 @@ source ./scripts/common.sh
 for project in ${PROJECTS}
 do
     echo "${project}"
-    # pushd "${project}"
-    #     pulumi login
-    #     pulumi stack select dev
-    #     pulumi up --yes --skip-preview --non-interactive
-    # popd
+    pushd "${project}"
+        pulumi login
+        stack="pulumi-in-action/${project/\//-}/dev"
+        pulumi stack init ${stack} || true
+        pulumi stack select ${stack}
+        pulumi up --yes --skip-preview --non-interactive
+    popd
 done
